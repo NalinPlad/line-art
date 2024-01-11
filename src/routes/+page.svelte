b695751ceac016ce77ceec0a4c576b14b5f24842
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2024-01-10 21:35:24 -0800

    Drawing!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,93 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { spring } from 'svelte/motion';
+    let lines = [];
+    let drawing = false;
+    let startPoint = {};
+    let rect;
+
+    const segment_length = 5;
+
+    let endSizes = spring(
+        {
+            L: 1,
+            R: 1,
+        },
+        {
+            stiffness: 0.1,
+            damping: 0.2,
+        }
+    );
+
+    function serialize(lines) {
+        return lines.map(line => `${line.x1},${line.y1},${line.x2},${line.y2}`).join(' ');
+    }
+
+
+
+    function startDrawing(event) {
+        rect = document.getElementById("svg").getBoundingClientRect();
+        drawing = true;
+        startPoint.x = event.clientX - rect.left;
+        startPoint.y = event.clientY - rect.top;
+    }
+
+    function drawLine(event) {
+        const endPoint = {
+            x: event.clientX - rect.left,
+            y: event.clientY - rect.top,
+        };
+
+        // if close enough to either end circle, make it smaller, and snap to it
+        if (Math.abs(endPoint.x) < 20 && Math.abs(endPoint.y - 200) < 20) {
+            endSizes.set({L: 2, R: 1})
+            endPoint.x = 0;
+            endPoint.y = 200;
+        } else if (Math.abs(endPoint.x - 600) < 20 && Math.abs(endPoint.y - 200) < 20) {
+            endSizes.set({L: 1, R: 2})
+            endPoint.x = 600;
+            endPoint.y = 200;
+        } else {
+            endSizes.set({L: 1, R: 1})
+        }
+
+
+
+        if (!drawing) return;
+        // console.log(`Endpoint: ${endPoint.x},${endPoint.y}, cXY: ${event.clientX}, ${event.clientY}, rect: ${rect.left}, ${rect.top}`);
+
+        // if more than 5 pixels away from the last point
+        if (Math.abs(endPoint.x - startPoint.x) > segment_length || Math.abs(endPoint.y - startPoint.y) > segment_length) {
+
+            lines = [...lines, { x1: startPoint.x, y1: startPoint.y, x2: endPoint.x, y2: endPoint.y }];
+            // console.log(`drew line, ${startPoint.x}, ${startPoint.y}}, ${endPoint.x}, ${endPoint.y}`);
+            startPoint = endPoint;
+        } else {
+            // console.log("didn't draw line");
+        }
+
+
+
+        //   lines = [...lines, { x1: startPoint.x, y1: startPoint.y, x2: endPoint.x, y2: endPoint.y }];
+    }
+
+    function stopDrawing() {
+        drawing = false;
+    }
+</script>
+
+<style>
+    svg {
+        /* border: 1px solid #ccc; */
+        cursor: crosshair;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+</style>
+
+<svg width="600" height="400" on:mousedown={startDrawing} on:mousemove={drawLine} on:mouseup={stopDrawing} on:mouseleave={stopDrawing} id="svg">
+    {#each lines as line}
+        <line x1={line.x1} y1={line.y1} x2={line.x2} y2={line.y2} stroke="black" stroke-width="4" stroke-linecap="round" />
+    {/each}
+    <!--Circles at each horizontal end of the box, in the middle vertically-->
+    <circle cx="0" cy="200" r={5*$endSizes.L} fill="black" />
+    <circle cx="600" cy="200" r={5*$endSizes.R} fill="black" />
+</svg>
