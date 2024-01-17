60b716ae62e872ac01d2448f0ed46eb2968c0950
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2024-01-16 19:28:10 -0800

    fix import statement that can be relative

--- rust/src/drawings.rs
+++ rust/src/drawings.rs
@@ -1,14 +1,13 @@
 use actix_web::{delete, get, post, put, web, HttpResponse, Responder};
 use crate::models::*;
 
-
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
 #[post("/submit")]
-async fn submit(req_body: web::Json<crate::models::DrawingRequest>) -> impl Responder {
+async fn submit(req_body: web::Json<DrawingRequest>) -> impl Responder {
     let line_data = LineData::from_drawing_request(req_body.into_inner());
 
     if !line_data.validate_line() {
